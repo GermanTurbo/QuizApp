d586630bb3486298d80eb6e459873c765277f29c
GermanTurbo <tobiaspalme2003@gmail.com>
2022-08-05 16:13:47 +0200

    added Material3 Theme and Colors

--- app/src/main/java/com/tp/quizapp/MainActivity.java
+++ app/src/main/java/com/tp/quizapp/MainActivity.java
@@ -23,6 +23,7 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 
+
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
@@ -49,9 +50,9 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.bottomNavigationView, navController);
+        System.out.println("hallo");
 
 
-        //toolbar = getSupportActionBar();
         //bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavigationView);
         //bottomNavigationView.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
 
